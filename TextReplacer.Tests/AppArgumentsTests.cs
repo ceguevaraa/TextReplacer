81d80880dd587fcad3b16cd88e887351d1e10e68
= <=>
2025-10-16 18:57:04 +0200

    code clean up

--- TextReplacer.Tests/AppArgumentsTests.cs
+++ TextReplacer.Tests/AppArgumentsTests.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TextReplacer.Models;
+﻿using TextReplacer.Models;
 using FluentAssertions;
-using Xunit;
 
 namespace TextReplacer.Tests
 {
